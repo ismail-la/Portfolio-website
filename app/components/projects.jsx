48f4e1ccd8cdb43b1b18d877b464eb8410ceb2ec
ismail-la <lahbariismail@gmail.com>
2024-08-14 20:05:44 +0100

    update projects

---
+++ app/components/projects.jsx
@@ -0,0 +1,168 @@
+"use client";
+
+import Image from "next/image";
+import "./Projects.css";
+import project1 from "../../public/imgs/project-1.png";
+import project2 from "../../public/imgs/project-2.png";
+import project3 from "../../public/imgs/project-3.png";
+import project4 from "../../public/imgs/project-4.png";
+import project5 from "../../public/imgs/project-5.png";
+import project6 from "../../public/imgs/project-6.png";
+import project7 from "../../public/imgs/project-7.png";
+import { BsFillArrowUpRightCircleFill } from "react-icons/bs";
+import { useRef } from "react";
+import { useInView } from "framer-motion";
+
+const Projects = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+  return (
+    <section id="projects" className="projects">
+      <svg width="0" height="0">
+        <linearGradient id="gradient" x1="100%" y1="100%" x2="0%" y2="0%">
+          <stop stopColor="#3f5efb" offset="0%" />
+          <stop stopColor="#fc466b" offset="100%" />
+        </linearGradient>
+      </svg>
+
+      <h1 ref={ref}>Projects</h1>
+      <div className="projects-container">
+        <div
+          className="projects-image"
+          style={{
+            transform: isInView ? "none" : "translateX(-200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+          }}
+        >
+          <Image src={project1} width={0} height={0} />
+          <a href="https://github.com/ismail-la/E-commerce-MVP" target="_blank">
+            <div className="icon">
+              <BsFillArrowUpRightCircleFill
+                size={100}
+                style={{ fill: "url(#gradient)" }}
+              />
+              <h3>Check out Project</h3>
+            </div>
+          </a>
+        </div>
+        <div
+          className="projects-image"
+          style={{
+            transform: isInView ? "none" : "translateX(200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+          }}
+        >
+          <Image src={project2} width={0} height={0} />
+          <a href="https://github.com/ismail-la/AirBnB_clone" target="_blank">
+            <div className="icon">
+              <BsFillArrowUpRightCircleFill
+                size={100}
+                style={{ fill: "url(#gradient)" }}
+              />
+              <h3>Check out Project</h3>
+            </div>
+          </a>
+        </div>
+        <div
+          className="projects-image"
+          style={{
+            transform: isInView ? "none" : "translateX(-200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+          }}
+        >
+          <Image src={project3} width={0} height={0} />
+          <a href="" target="_blank">
+            <div className="icon">
+              <BsFillArrowUpRightCircleFill
+                size={100}
+                style={{ fill: "url(#gradient)" }}
+              />
+              <h3>Check out Project</h3>
+            </div>
+          </a>
+        </div>
+        <div
+          className="projects-image"
+          style={{
+            transform: isInView ? "none" : "translateX(200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+          }}
+        >
+          <Image src={project4} width={0} height={0} />
+          <a href="" target="_blank">
+            <div className="icon">
+              <BsFillArrowUpRightCircleFill
+                size={100}
+                style={{ fill: "url(#gradient)" }}
+              />
+              <h3>Check out Project</h3>
+            </div>
+          </a>
+        </div>
+        <div
+          className="projects-image"
+          style={{
+            transform: isInView ? "none" : "translateX(-200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+          }}
+        >
+          <Image src={project5} width={0} height={0} />
+          <a href="" target="_blank">
+            <div className="icon">
+              <BsFillArrowUpRightCircleFill
+                size={100}
+                style={{ fill: "url(#gradient)" }}
+              />
+              <h3>Check out Project</h3>
+            </div>
+          </a>
+        </div>
+        <div
+          className="projects-image"
+          style={{
+            transform: isInView ? "none" : "translateX(200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+          }}
+        >
+          <Image src={project6} width={0} height={0} />
+          <a href="" target="_blank">
+            <div className="icon">
+              <BsFillArrowUpRightCircleFill
+                size={100}
+                style={{ fill: "url(#gradient)" }}
+              />
+              <h3>Check out Project</h3>
+            </div>
+          </a>
+        </div>
+        <div
+          className="projects-image"
+          style={{
+            transform: isInView ? "none" : "translateX(200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+          }}
+        >
+          <Image src={project7} width={0} height={0} />
+          <a href="" target="_blank">
+            <div className="icon">
+              <BsFillArrowUpRightCircleFill
+                size={100}
+                style={{ fill: "url(#gradient)" }}
+              />
+              <h3>Check out Project</h3>
+            </div>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
